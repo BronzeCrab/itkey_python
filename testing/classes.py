5bb85f6e98e2e622adfc413ea0730883a4e2418e
Alexey Ustinnikov <gr4k@bk.ru>
2015-04-22 17:08:43 +0300

    tasks from Artem

---
+++ testing/classes.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+import socket
+
+# "Second variant of script - using classes"
+
+# Classes with inheretence from object - python new-style classes.
+
+class Date_and_ip(object):
+
+	version = "Version of script is 1.0"
+
+	def get_current_time(self):
+		now = datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S")
+		return now
+
+	def get_ip(self):
+		# The AF_INET address family is the address family for IPv4.
+		#create an INET, STREAMing socket, SOCK_DGRAM Supports unreliable connectionless datagram communication.
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		s.connect(('8.8.8.8', 0))  # connecting to a UDP address doesn't send packets
+		local_ip_address = s.getsockname()[0]
+		return local_ip_address
+
+class Child(Date_and_ip):
+	def __init__(self):
+		# In this variant it searches for global variable ( if i do just 'print version')
+		# print version
+		# But 'super' allows to connect to parent classes without explicit call of them
+		print super(Child, self).version
+		print super(Child, self).get_current_time()
+		print super(Child, self).get_ip()
+
+test = Child()
